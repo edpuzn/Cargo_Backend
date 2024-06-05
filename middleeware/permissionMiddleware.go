91ddc421f9873ed2ebac042745554f6aa9aa55f6
Edip UZAN <edpuzn@gmail.com>
2024-06-05 14:24:20 +0300

    Initial commit

---
+++ middleeware/permissionMiddleware.go
@@ -0,0 +1,56 @@
+package middleeware
+
+import (
+	"Cargo_Dash/database"
+	"Cargo_Dash/models"
+	"Cargo_Dash/util"
+	"errors"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"strconv"
+)
+
+func IsAuthorized(c *fiber.Ctx, page string) error {
+	cookie := c.Cookies("jwt")
+
+	Id, err := util.ParseJWT(cookie)
+
+	fmt.Println(Id)
+
+	if err != nil {
+		return err
+	}
+
+	userId, _ := strconv.Atoi(Id)
+
+	user := models.User{
+		Id: uint(userId),
+	}
+
+	database.DB.Preload("Role").Find(&user)
+
+	role := models.Role{
+		Id: user.RoleId,
+	}
+
+	database.DB.Preload("Permissions").Find(&role)
+
+	fmt.Println(role.Permissions)
+	if c.Method() == "GET" {
+		for _, permission := range role.Permissions {
+			if permission.Name == "view_"+page || permission.Name == "edit_"+page {
+				return nil
+			}
+		}
+	} else {
+		for _, permission := range role.Permissions {
+			fmt.Println(permission.Name)
+			if permission.Name == "edit_"+page {
+				return nil
+			}
+		}
+	}
+
+	c.Status(fiber.StatusUnauthorized)
+	return errors.New("unauthorized")
+}
